241e9c70a91cf90fc91ae6f12a6133d5f8ee0a04
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-09-16 19:52:54 +0100

    added a lot of things the gameobject.py

--- mandaw/gameobject.py
+++ mandaw/gameobject.py
@@ -1,17 +1,45 @@
 import sdl2
 import sdl2.ext
+from mandawsdl.color import Color
 
 class GameObject(sdl2.ext.Entity):
-    def __init__(self, world, size = (0, 0), x = 0, y = 0, color = sdl2.ext.Color(255, 255, 255)):
-        self.size = size
-        self.x = x
-        self.y = y
-        
+    def __init__(self, world, width = 20, height = 20, x = 0, y = 0, color = Color(255, 255, 255)):
         self.world = world
+
+        self.position = Position(x, y)
+        self.size = Size(width, height)
+
+        self.attribute = Attribute()
 
         self.color = color
 
-        factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE)
-        self.sprite = factory.from_color(self.color, size)
+        self.sprite = self.world.factory.from_color(self.color, (self.size.width, self.size.height))
+    
+    def draw(self):
+        self.sprite.position = self.position.x, self.position.y
 
-        self.sprite.position = x, y
+    def collide(self, rect):
+        left, top, right, bottom = self.sprite.area
+        bleft, btop, bright, bbottom = rect.sprite.area
+
+        return(bleft < right and bright > left and btop < bottom and bbottom > top)
+
+    def center(self):
+        self.position.x = int(self.world.width / 2)
+        self.position.y = int(self.world.height / 2)
+
+class Position(object):
+    def __init__(self, x, y):
+        self.position = x, y
+        self.x = x
+        self.y = y
+
+class Size(object):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.size = (self.width, self.height)
+
+class Attribute(object):
+    def __init__(self):
+        pass
