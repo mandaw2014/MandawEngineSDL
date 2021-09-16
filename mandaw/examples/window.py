281b9ef37debaffd52616a589167d1b861bd370c
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-09-16 19:57:07 +0100

    fixed window.py

--- mandaw/examples/window.py
+++ mandaw/examples/window.py
@@ -2,7 +2,9 @@ from mandawsdl import *
 
 mandaw = Mandaw("Mandaw", 800, 600)
 
-square = GameObject(mandaw.world, (20, 20), 390, 290)
+square = GameObject(mandaw.world, width = 20, height = 20, color = Color(255, 0, 0))
+square.center()
 
 while True:
+    square.draw()
     mandaw.run()
