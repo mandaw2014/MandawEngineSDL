da6eb1082347a7e0843369b54fa8d1cb36bb0169
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-09-18 15:19:42 +0100

    Delete mandawsdl directory

--- mandawsdl/__init__.py
+++
@@ -1,5 +0,0 @@
-from mandawsdl.main import Mandaw
-from mandawsdl.gameobject import GameObject
-from mandawsdl.color import Color
-from mandawsdl.input import Input
-from mandawsdl.text import Text
--- mandawsdl/color.py
+++
@@ -1,7 +0,0 @@
-import sdl2.ext
-
-class Color(sdl2.ext.Color):
-    def __init__(self, r, g, b):
-        super().__init__(
-            r, g, b
-        )
