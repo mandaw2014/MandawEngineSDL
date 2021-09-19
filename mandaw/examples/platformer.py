a24b27be98f26065d4dd3f06ae48a1ef005011ba
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-09-19 11:53:15 +0100

    fixed platformer.py

--- mandaw/examples/platformer.py
+++ mandaw/examples/platformer.py
@@ -3,10 +3,12 @@ from mandaw import *
 mandaw = Mandaw("Platformer!", width = 800, height = 600)
 mandaw.bg_color = mandaw.color["cyan"]
 
+objects = []
+
 class PlatformerController2D(GameObject):
-    def __init__(self, world, x = 0, y = 0, centered = True):
+    def __init__(self, x = 0, y = 0, centered = True):
         super().__init__(
-            world,
+            mandaw,
             width = 15,
             height = 35,
             x = x,
@@ -14,112 +16,125 @@ class PlatformerController2D(GameObject):
             color = Color(255, 165, 0)
         )
 
-        self.attribute.window = world
-
         if centered == True:
             self.center()
         else:
             pass
 
         # Player's X Position
-        self.attribute.pos_x = 0
+        self.pos_x = 0
 
         # Set the position as a variable
-        self.attribute.pos = self.attribute.window.width / 2 - 15
+        self.pos = self.window.width / 2 - 15
 
-        self.attribute.is_jumping = False
-        self.attribute.jump_y = 5
+        self.is_jumping = False
+        self.jump_y = 5
 
-        self.attribute.direction = None
+        self.direction = None
 
-        self.attribute.velocity_y = 1
+        self.velocity_y = 1
 
         # Player's speed and maxspeed
-        self.attribute.speed = 0.1
-        self.attribute.maxspeed = 2
+        self.speed = 0.1
+        self.maxspeed = 2
 
-        self.attribute.ground = None
-        self.attribute.ignore_speed = []
+        self.ignore_speed = []
 
     def movement(self):
         # Player movement
         if mandaw.input.pressed[mandaw.input.keys["A"]]:
-            self.attribute.pos_x -= self.attribute.speed * mandaw.dt
-            self.attribute.direction = 0
+            self.pos_x -= self.speed * mandaw.dt
+            self.direction = 0
 
         if mandaw.input.pressed[mandaw.input.keys["D"]]:
-            self.attribute.pos_x += self.attribute.speed * mandaw.dt
-            self.attribute.direction = 1
+            self.pos_x += self.speed * mandaw.dt
+            self.direction = 1
 
         # Momentum
-        if self.attribute.pos_x >= self.attribute.maxspeed:
-            self.attribute.pos_x = self.attribute.maxspeed
-        if self.attribute.pos_x <= -self.attribute.maxspeed:
-            self.attribute.pos_x = -self.attribute.maxspeed
+        if self.pos_x >= self.maxspeed:
+            self.pos_x = self.maxspeed
+        if self.pos_x <= -self.maxspeed:
+            self.pos_x = -self.maxspeed
 
-        self.attribute.pos += int(self.attribute.pos_x)
+        self.pos += int(self.pos_x)
 
         # Gravity
-        if not self.collide(self.attribute.ground) and self.attribute.is_jumping == False:
-            self.position.y += 3 * int(self.attribute.velocity_y)
-            self.attribute.velocity_y += 0.1
+        if not self.collide(objects) and self.is_jumping == False:
+            self.y += 3 * int(self.velocity_y)
+            self.velocity_y += 0.1
 
-        if self.collide(self.attribute.ground):
-            self.attribute.velocity_y = 1
+        if self.collide(objects):
+            self.velocity_y = 1
 
-            if self.attribute.direction == 0 and not mandaw.input.pressed[mandaw.input.keys["A"]]:
-                self.attribute.pos_x += 10
+            if self.direction == 0 and not mandaw.input.pressed[mandaw.input.keys["A"]]:
+                self.pos_x += 10
 
-                if self.attribute.pos_x >= 0:
-                    self.attribute.pos_x = 0
+                if self.pos_x >= 0:
+                    self.pos_x = 0
             
-            if self.attribute.direction == 1 and not mandaw.input.pressed[mandaw.input.keys["D"]]:
-                self.attribute.pos_x -= 10
+            if self.direction == 1 and not mandaw.input.pressed[mandaw.input.keys["D"]]:
+                self.pos_x -= 10
 
-                if self.attribute.pos_x <= 0:
-                    self.attribute.pos_x = 0
+                if self.pos_x <= 0:
+                    self.pos_x = 0
 
-        if not self.collide(self.attribute.ground):
-            if self.attribute.direction == 0 and not mandaw.input.pressed[mandaw.input.keys["A"]]:
-                self.attribute.pos_x += 0.1 * mandaw.dt
+        if not self.collide(objects):
+            if self.direction == 0 and not mandaw.input.pressed[mandaw.input.keys["A"]]:
+                self.pos_x += 0.1 * mandaw.dt
 
-                if self.attribute.pos_x >= 0:
-                    self.attribute.pos_x = 0
+                if self.pos_x >= 0:
+                    self.pos_x = 0
             
-            if self.attribute.direction == 1 and not mandaw.input.pressed[mandaw.input.keys["D"]]:
-                self.attribute.pos_x -= 0.1 * mandaw.dt
+            if self.direction == 1 and not mandaw.input.pressed[mandaw.input.keys["D"]]:
+                self.pos_x -= 0.1 * mandaw.dt
 
-                if self.attribute.pos_x <= 0:
-                    self.attribute.pos_x = 0
+                if self.pos_x <= 0:
+                    self.pos_x = 0
 
         # Set the x position as the x variable
-        self.position.x = int(self.attribute.pos)
+        self.x = int(self.pos)
 
         self.jump()
 
     def jump(self):
         # Jumping
-        if self.attribute.is_jumping == False and mandaw.input.pressed[mandaw.input.keys["SPACE"]]:
-            self.attribute.is_jumping = True
-            if not self.collide(self.attribute.ground):
-                self.attribute.is_jumping = False
+        if self.is_jumping == False and mandaw.input.pressed[mandaw.input.keys["SPACE"]]:
+            self.is_jumping = True
+            if not self.collide(objects):
+                self.is_jumping = False
 
-        if self.attribute.is_jumping == True:
-            self.position.y -= self.attribute.jump_y
-            self.attribute.jump_y -= 1
+        if self.is_jumping == True:
+            self.y -= self.jump_y
+            self.jump_y -= 1
 
-            if self.attribute.jump_y < -5:
-                self.attribute.is_jumping = False
-                self.attribute.jump_y = 10
+            if self.jump_y < -5:
+                self.is_jumping = False
+                self.jump_y = 10
 
-player = PlatformerController2D(mandaw.world, 0, 0, True)
+class Platform(GameObject):
+    def __init__(self, x, y):
+        super().__init__(
+            mandaw,
+            width = 50,
+            height = 10,
+            x = x,
+            y = y,
+            color = Color(255, 255, 0)
+        )
 
-ground = GameObject(mandaw.world, 5000, 100, 0, 500, color = Color(195, 195, 195))
+player = PlatformerController2D(0, 0, True)
+
+ground = GameObject(mandaw, 5000, 100, 0, 500, color = Color(195, 195, 195))
 ground.center_x()
 
-player.attribute.ground = ground
+platform = Platform(0, 450)
+platform.center_x()
+
+objects.append(platform)
+objects.append(platform)
 
 while True:
+    platform.draw()
     player.draw()
     ground.draw()
 
