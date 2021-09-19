97e63efc598dc38e90ce9658fc76ce0b3e5a4d21
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-09-19 11:52:11 +0100

    fixed a lot of things in gameobject.py

--- mandaw/gameobject.py
+++ mandaw/gameobject.py
@@ -2,58 +2,48 @@ import sdl2
 import sdl2.ext
 from mandaw.color import Color
 
-class GameObject(sdl2.ext.Entity):
-    def __init__(self, world, width = 20, height = 20, x = 0, y = 0, color = Color(255, 255, 255)):
-        self.world = world
+class GameObject(object):
+    def __init__(self, window, width = 20, height = 20, x = 0, y = 0, color = Color(255, 255, 255)):
+        self.entity = sdl2.ext.Entity(world = window.world)
 
-        self.position = Position(x, y)
-        self.size = Size(width, height)
-
-        self.attribute = Attribute()
-        self.attribute.color = color
-
-        self.sprite = self.world.factory.from_color(self.attribute.color, (0, 0))
-    
-    def draw(self):
-        self.sprite = self.world.factory.from_color(self.attribute.color, (self.size.width, self.size.height))
-        self.sprite.position = self.position.x, self.position.y
-
-    def collide(self, rect):
-        if type(rect) != list:
-            left, top, right, bottom = self.sprite.area
-            bleft, btop, bright, bbottom = rect.sprite.area
-
-            return(bleft < right and bright > left and btop < bottom and bbottom > top)
-        
-        elif type(rect) == list:
-            for i in range(len(rect)):
-                left, top, right, bottom = self.sprite.area
-                bleft, btop, bright, bbottom = rect[i].sprite.area
-
-                return(bleft < right and bright > left and btop < bottom and bbottom > top)
-
-    def center(self):
-        self.position.x = int(self.world.width / 2) - int(self.size.width / 2)
-        self.position.y = int(self.world.height / 2) - int(self.size.height / 2)
-
-    def center_x(self):
-        self.position.x = int(self.world.width / 2) - int(self.size.width / 2)
-
-    def center_y(self):
-        self.position.y = int(self.world.height / 2) - int(self.size.height / 2)
-
-class Position(object):
-    def __init__(self, x, y):
-        self.position = x, y
+        self.window = window
+        self.width = width
+        self.height = height
         self.x = x
         self.y = y
 
-class Size(object):
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.size = (self.width, self.height)
+        self.color = color
 
-class Attribute(object):
-    def __init__(self):
-        pass
+        self.entity.sprite = self.entity.world.factory.from_color(self.color, (0, 0))
+    
+    def draw(self):
+        self.entity.sprite = self.entity.world.factory.from_color(self.color, (self.width, self.height))
+        self.entity.sprite.position = self.x, self.y
+
+    def collide(self, rect):
+        left, top, right, bottom = self.entity.sprite.area
+
+        try:
+            if type(rect) != list:
+                bleft, btop, bright, bbottom = rect.entity.sprite.area
+
+                return(bleft < right and bright > left and btop < bottom and bbottom > top)
+            
+            elif type(rect) == list:
+                for i in range(len(rect)):
+                    bleft, btop, bright, bbottom = rect[i].entity.sprite.area
+
+                    return(bleft < right and bright > left and btop < bottom and bbottom > top)
+
+        except:
+            raise AttributeError("MandawError: Object was not a GameObject or a list of GameObjects :(")
+
+    def center(self):
+        self.x = int(self.window.width / 2) - int(self.width / 2)
+        self.y = int(self.window.height / 2) - int(self.height / 2)
+
+    def center_x(self):
+        self.x = int(self.window.width / 2) - int(self.width / 2)
+
+    def center_y(self):
+        self.y = int(self.window.height / 2) - int(self.height / 2)
