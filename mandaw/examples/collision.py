2656757058dd9a2497b1ce2a8eb5b0fb2b8d85c2
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-09-19 11:52:57 +0100

    fixed collision.py

--- mandaw/examples/collision.py
+++ mandaw/examples/collision.py
@@ -2,56 +2,53 @@ from mandaw import *
 
 mandaw = Mandaw("Collisions!", 800, 600)
 
-player = GameObject(mandaw.world, 30, 30, 0, 0)
+player = GameObject(mandaw, 30, 30, 0, 0)
 player.center()
 
 class CollisionObject(GameObject):
-    def __init__(self, world, x, y):
+    def __init__(self, x, y):
         super().__init__(
-            world,
+            mandaw,
             width = 25,
             height = 25,
             x = x,
             y = y
         )
 
-object1 = CollisionObject(mandaw.world, 600, 300)
-object2 = CollisionObject(mandaw.world, 200, 300)
-object3 = CollisionObject(mandaw.world, 400, 100)
-object4 = CollisionObject(mandaw.world, 400, 400)
+object1 = CollisionObject(600, 300)
+object2 = CollisionObject(200, 300)
+object3 = CollisionObject(400, 100)
+object4 = CollisionObject(400, 400)
 
 while True:
     if mandaw.input.pressed[mandaw.input.keys["UP"]]:
-        player.position.y -= 1
+        player.y -= 1
     if mandaw.input.pressed[mandaw.input.keys["LEFT"]]:
-        player.position.x -= 1
+        player.x -= 1
     if mandaw.input.pressed[mandaw.input.keys["DOWN"]]:
-        player.position.y += 1
+        player.y += 1
     if mandaw.input.pressed[mandaw.input.keys["RIGHT"]]:
-        player.position.x += 1
-
-    if player.collide(object1):
-        object1.attribute.color = Color(255, 0, 0)
-    else:
-        object1.attribute.color = Color(255, 255, 255)
-
-    if player.collide(object2):
-        object2.attribute.color = Color(255, 0, 0)
-    else:
-        object2.attribute.color = Color(255, 255, 255)
-
-    if player.collide(object3):
-        object3.attribute.color = Color(255, 0, 0)
-    else:
-        object3.attribute.color = Color(255, 255, 255)
-
-    if player.collide(object4):
-        object4.attribute.color = Color(255, 0, 0)
-    else:
-        object4.attribute.color = Color(255, 255, 255)
+        player.x += 1
 
     if mandaw.input.pressed[mandaw.input.keys["G"]]:
         player.center()
+
+    if player.collide(object1):
+        object1.color = Color(255, 0, 0)
+    else:
+        object1.color = Color(255, 255, 255)
+    if player.collide(object2):
+        object2.color = Color(255, 0, 0)
+    else:
+        object2.color = Color(255, 255, 255)
+    if player.collide(object3):
+        object3.color = Color(255, 0, 0)
+    else:
+        object3.color = Color(255, 255, 255)
+    if player.collide(object4):
+        object4.color = Color(255, 0, 0)
+    else:
+        object4.color = Color(255, 255, 255)
 
     object1.draw()
     object2.draw()
