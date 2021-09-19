31e8c8eea583304487b6591d957dd41f8ee8f3d2
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-09-19 11:52:40 +0100

    fixed class.py

--- mandaw/examples/class.py
+++ mandaw/examples/class.py
@@ -3,9 +3,9 @@ from mandaw import *
 mandaw = Mandaw("Classes!", 800, 600)
 
 class Cube(GameObject):
-    def __init__(self, world):
+    def __init__(self):
         super().__init__(
-            world = world,
+            window = mandaw,
             width = 20,
             height = 20,
             x = 0,
@@ -15,7 +15,7 @@ class Cube(GameObject):
 
         self.center()
 
-cube = Cube(mandaw.world)
+cube = Cube()
 
 while True:
     cube.draw()
