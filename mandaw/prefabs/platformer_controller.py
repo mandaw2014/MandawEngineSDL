3e1f17b490bfaa5090fcc3ad2d68f6c28dbe60d2
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-10-05 20:26:00 +0100

    made prefabs folder

---
+++ mandaw/prefabs/platformer_controller.py
@@ -0,0 +1,129 @@
+from mandaw import Mandaw, GameObject, Color
+
+class PlatformerController2D(GameObject):
+    def __init__(self, window, x = 0, y = 0, centered = True):
+        super().__init__(
+            window,
+            width = 15,
+            height = 35,
+            x = x,
+            y = y,
+            color = Color(255, 165, 0)
+        )
+
+        if centered == True:
+            self.center()
+        else:
+            pass
+        
+        # Window
+        self.window = window
+
+        # Player's X Position
+        self.pos_x = 0
+
+        # Set the position as a variable
+        self.pos = self.window.width / 2 - 15
+
+        self.is_jumping = False
+        self.jump_y = 5
+
+        self.direction = None
+
+        self.velocity_y = 1
+
+        self.objects = []
+
+        # Player's speed and maxspeed
+        self.speed = 0.1
+        self.maxspeed = 2
+
+        self.ignore_speed = []
+
+    def movement(self):
+        # Player movement
+        if self.window.input.pressed[self.window.input.keys["A"]]:
+            self.pos_x -= self.speed
+            self.direction = 0
+
+        if self.window.input.pressed[self.window.input.keys["D"]]:
+            self.pos_x += self.speed
+            self.direction = 1
+
+        # Momentum
+        if self.pos_x >= self.maxspeed:
+            self.pos_x = self.maxspeed
+        if self.pos_x <= -self.maxspeed:
+            self.pos_x = -self.maxspeed
+
+        self.pos += self.pos_x
+
+        # Gravity
+        if not self.collidelist(self.objects) and self.is_jumping == False:
+            self.y += 3 * int(self.velocity_y)
+            self.velocity_y += 0.1 * self.window.dt
+
+        if self.collidelist(self.objects):
+            self.velocity_y = 1
+
+            if self.direction == 0 and not self.window.input.pressed[self.window.input.keys["A"]]:
+                self.pos_x += 10
+
+                if self.pos_x >= 0:
+                    self.pos_x = 0
+            
+            if self.direction == 1 and not self.window.input.pressed[self.window.input.keys["D"]]:
+                self.pos_x -= 10
+
+                if self.pos_x <= 0:
+                    self.pos_x = 0
+
+        if not self.collidelist(self.objects):
+            if self.direction == 0 and not self.window.input.pressed[self.window.input.keys["A"]]:
+                self.pos_x += 0.1
+
+                if self.pos_x >= 0:
+                    self.pos_x = 0
+            
+            if self.direction == 1 and not self.window.input.pressed[self.window.input.keys["D"]]:
+                self.pos_x -= 0.1
+
+                if self.pos_x <= 0:
+                    self.pos_x = 0
+
+        # Set the x position as the x variable
+        self.x = int(self.pos)
+
+        self.jump()
+
+    def jump(self):
+        # Jumping
+        if self.is_jumping == False and self.window.input.pressed[self.window.input.keys["SPACE"]]:
+            self.is_jumping = True
+            if not self.collidelist(self.objects):
+                self.is_jumping = False
+
+        if self.is_jumping == True:
+            self.y -= self.jump_y
+            self.jump_y -= 1
+
+            if self.jump_y < -5:
+                self.is_jumping = False
+                self.jump_y = 10
+
+if __name__ == "__main__":
+    mandaw = Mandaw("PlatformerController2D!", 800, 600, Color(0, 255, 255))
+
+    player = PlatformerController2D(mandaw, 0, 0, True)
+
+    ground = GameObject(mandaw, 5000, 100, 0, 500, Color(150, 150, 150))
+    ground.center_x()
+
+    player.objects.append(ground)
+
+    while True:
+        player.movement()
+
+        player.draw()
+        ground.draw()
+        mandaw.run()
