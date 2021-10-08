a97fefbccdaf3ade18c358f1f7c4877090576f42
Mandaw <77012627+mandaw2014@users.noreply.github.com>
2021-10-08 18:00:51 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(name='Mandaw',
-      version='2.0.1',
+      version='0.0.1',
       description='A 2D Python GameEngine Made With PySDL2',
       author='mandaw2014',
       keywords="python 2d game development",
       author_email='mandawbuisness@gmail.com',
-      url='https://github.com/mandaw2014/MandawEngine',
+      url='https://github.com/mandaw2014/MandawEngineSDL/tree/experimental',
       packages=['mandaw'],
       package_dir={'mandaw': 'mandaw'},
       install_requires=["pysdl2"]
